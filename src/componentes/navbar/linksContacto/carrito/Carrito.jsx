c594efd73cd57f1dda961daf2274cb5a9c6b1a27
dylan <devdylancrowder@outlook.com>
2023-07-28 14:12:42 -0300

    problema suma carrito resuelto

--- src/componentes/navbar/linksContacto/carrito/Carrito.jsx
+++ src/componentes/navbar/linksContacto/carrito/Carrito.jsx
@@ -1,10 +1,10 @@
 import "./carrito.css";
-import { useCartLogic } from "../../../verMas/logicaCarta";
+
 import React, { useContext } from "react";
 import { CartContext } from "../../../../context/CartContext";
 
-const Carrito = ({ item }) => {
-    const { cantidad, restar, sumar, sumarCarrito } = useCartLogic(item);
+const Carrito = () => {
+
     const { carrito, carritoTotal, setCarrito } = useContext(CartContext);
 
     const eliminarCarrito = () => {
@@ -12,30 +12,73 @@ const Carrito = ({ item }) => {
         setCarrito([]);
     };
 
+    const restarAca = (prodId) => {
+
+
+        // Encuentra el producto en el carrito por su ID
+        const productoEnCarrito = carrito.find((prod) => prod.id === prodId);
+        if (!productoEnCarrito) return; // Salir si el producto no se encuentra en el carrito
+
+        // Si la cantidad es mayor a 1, resta uno; de lo contrario, elimina el producto del carrito
+        if (productoEnCarrito.cantidad > 1) {
+            const nuevoCarrito = carrito.map((prod) =>
+                prod.id === prodId ? { ...prod, cantidad: prod.cantidad - 1 } : prod
+            );
+            setCarrito(nuevoCarrito);
+        } else {
+            const nuevoCarrito = carrito.filter((prod) => prod.id !== prodId);
+            setCarrito(nuevoCarrito);
+        }
+    };
+
+    const sumarAca = (prodId) => {
+
+
+        const productoEnCarrito = carrito.find((prod) => prod.id === prodId);
+        if (!productoEnCarrito) return; // Salir si el producto no se encuentra en el carrito
+
+        // Si la cantidad es mayor a 1, resta uno; de lo contrario, elimina el producto del carrito
+        if (productoEnCarrito.cantidad > 1) {
+            const nuevoCarrito = carrito.map((prod) =>
+                prod.id === prodId ? { ...prod, cantidad: prod.cantidad + 1 } : prod
+            );
+            setCarrito(nuevoCarrito);
+        } else {
+            const nuevoCarrito = carrito.filter((prod) => prod.id !== prodId);
+            setCarrito(nuevoCarrito);
+        }
+    }
+
     return (
         <div>
             <h1>Tu carro</h1>
 
             {carrito.map((prod) => (
                 <div className="padreCarrito" key={prod.id}>
+                    {console.log(prod.precio)}
                     <div className="columnas">
                         <div className="contenedorImgen">
-                            <img src={prod.img} alt={prod.categoria} />
-                        </div>
-                        <div className="contenedorInformacion">
-                            <h2>{prod.categoria}</h2>
-                            <p>${prod.precio}</p>
-                            <div>
-                                <button onClick={() => restar()}>-</button>
-                                <span>{cantidad}</span>
-                                <button onClick={() => sumar()}>+</button>
+                            <img className="carritoImagen"  src={prod.img} alt={prod.categoria} />
+                            <div className="contenedorInformacion">
+                                <h2>{prod.categoria}</h2>
+                                <p>${prod.precio}</p>
+
                             </div>
-                            <button onClick={() => sumarCarrito()}>sumar</button>
+
+                        </div>
+
+
+                        <div className="cantidad">
+                            <button className="sumarRestar" onClick={() => restarAca(prod.id)}>-</button>
+                            <p className="parrafoCantidad">{prod.cantidad}</p>
+                            <button className="sumarRestar" onClick={() => sumarAca(prod.id)}>+</button>
+                        </div>
+                        <div>
+                            <p>total</p>
                         </div>
                     </div>
                 </div>
             ))}
-
             <h2>precio total: {carritoTotal()}</h2>
             <button onClick={eliminarCarrito}>Eliminar carrito</button>
         </div>
